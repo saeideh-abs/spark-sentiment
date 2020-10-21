81b093b608b0b268faaef4f7a1708b29fcb0b4bd
saeideh abs <saeideh676@gmail.com>
2020-10-21 15:02:05 +0330

    classification using svm and tfidf vectors done

--- mobile.py
+++ mobile.py
@@ -1,8 +1,10 @@
 from __future__ import unicode_literals
-import hazm
 from hazm import *
 from pyspark.sql import SparkSession
 from pyspark import SparkContext
+from pyspark.ml.feature import HashingTF, IDF, Tokenizer
+from pyspark.ml.classification import LinearSVC
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 
 
 def get_info(df):
@@ -19,27 +21,48 @@ def get_info(df):
 
 
 def build_tfidf(docs):
-    tokens = custom_tokenizer(docs)
+    tokenizer = Tokenizer(inputCol="text", outputCol="tokens")
+    tokens = tokenizer.transform(docs)
+    # tokens = custom_tokenizer(docs)
+    hashingTF = HashingTF(inputCol="tokens", outputCol="hashedTf", numFeatures=300)
+    featurizedData = hashingTF.transform(tokens)
+    # featurizedData.select('rawFeatures').show(1, truncate=False)
 
-
-def tokenize(doc):
-    print(word_tokenize("sgh kjh al,"))
-    return doc
+    idf = IDF(inputCol="hashedTf", outputCol="hashedTfIdf")
+    idfModel = idf.fit(featurizedData)
+    rescaledData = idfModel.transform(featurizedData)
+    return rescaledData
 
 
 def custom_tokenizer(docs):
     # docs.show(5)
     # new = docs.rdd.map(str)
-    tokens = docs.rdd.map(lambda doc: tokenize(doc))
-    tokens.take(5)
+    tokens = docs.rdd.map(lambda doc: doc.text.split(' ')).toDF()
+    return tokens
+
+
+def svm_classification(train_df, test_df):
+    svm = LinearSVC(featuresCol='hashedTfIdf', labelCol='accept')
+    model = svm.fit(train_df)
+    model.setPredictionCol("prediction")
+    result_df = model.transform(test)
+    # result_df.select('prediction').show()
+    evaluator = MulticlassClassificationEvaluator(labelCol="accept", predictionCol="prediction",
+                                                  metricName="accuracy")
+    accuracy = evaluator.evaluate(result_df)
+    print("Test set accuracy = " + str(accuracy))
 
 
 if __name__ == '__main__':
     sc = SparkContext(appName="Mobile")
-    sc.addPyFile('./resources/hazm-master.zip')
+    # sc.addPyFile(hazm)
     spark = SparkSession.builder.master("local[1]").appName("Mobile").getOrCreate()
     data_df = spark.read.csv('./dataset/mobile_digikala.csv', inferSchema=True, header=True)
-    labels = data_df.select('accept')
-    comments = data_df.select('text')
+
     # get_info(data_df)
-    build_tfidf(comments)
+    featurized_df = build_tfidf(data_df)
+    featurized_df.printSchema()
+    train, test = featurized_df.randomSplit([0.9, 0.1])
+    print("train and test count", train.count(), test.count())
+    svm_classification(train, test)
+    spark.stop()
