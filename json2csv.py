0a195cc563309a82a00dd38f699af6e4dc863c6a
saeideh abs <saeideh676@gmail.com>
2021-01-12 10:19:25 +0330

    adding category column

--- json2csv.py
+++ json2csv.py
@@ -1,5 +1,13 @@
 import pandas as pd
 
 if __name__ == '__main__':
-    df = pd.read_json('dataset/headset.json')
-    df.to_csv('dataset/headset.csv', index=False)
+    df = pd.read_json('dataset/stationery.json')
+    df.to_csv('dataset/stationery.csv', index=False)
+
+    # codes for adding category column to mobile and tablet group
+    # df = pd.read_csv('dataset/mobile.csv')
+    # df = df.drop(['category'], axis=1)
+    # print(df.columns)
+    # # df['category'] = 'tablet'
+    # df.insert(2, 'category', 'موبایل')
+    # df.to_csv('dataset/mobile_cat.csv', index=False)
