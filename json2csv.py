aef94ae2d2846d7d1a40e5b56a0a751ddad2a9cc
saeideh abs <saeideh676@gmail.com>
2021-03-27 19:34:52 +0430

    make SentiFars lexicon positive and negative words

--- json2csv.py
+++ json2csv.py
@@ -1,8 +1,33 @@
 import pandas as pd
 
+
+def sentifars_lexicon():
+    df = pd.read_csv('./resources/SentiFars_lexicon/SentiFars_lexicon.csv')
+
+    positives = []
+    negatives = []
+    objectives = []
+    df['max'] = df[['positive', 'objective', 'negative']].idxmax(axis=1)
+    for index, polarity in enumerate(df['max']):
+        if polarity == 'positive':
+            positives.append(df.loc[index]['word'])
+        elif polarity == 'negative':
+            negatives.append(df.loc[index]['word'])
+        else:
+            objectives.append(df.loc[index]['word'])
+    pos = open('positive.txt', 'w')
+    [pos.write(elem + '\n') for elem in positives]
+
+    neg = open('negative.txt', 'w')
+    [neg.write(elem + '\n') for elem in negatives]
+
+    obj = open('objective.txt', 'w')
+    [obj.write(elem + '\n') for elem in objectives]
+
+
 if __name__ == '__main__':
-    df = pd.read_json('dataset/stationery.json')
-    df.to_csv('dataset/stationery.csv', index=False)
+    # df = pd.read_json('dataset/stationery.json')
+    # df.to_csv('dataset/stationery.csv', index=False)
 
     # codes for adding category column to mobile and tablet group
     # df = pd.read_csv('dataset/mobile.csv')
@@ -11,3 +36,5 @@ if __name__ == '__main__':
     # # df['category'] = 'tablet'
     # df.insert(2, 'category', 'موبایل')
     # df.to_csv('dataset/mobile_cat.csv', index=False)
+
+    sentifars_lexicon()
